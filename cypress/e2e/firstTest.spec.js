482dfd137e65bab62bdf2058a63371e4242a152e
alberto-odio-saferidehealth <alberto.odio@saferidehealth.com>
2025-01-11 23:20:30 -0600

    Update firstTest.spec.js

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -182,20 +182,39 @@ describe("First test suite", () => {
   });
 
   it.only("Date picker", () => {
+    function selectDayFromCurrent(day) {
+      let date = new Date();
+      date.setDate(date.getDate() + day);
+      let futureDay = date.getDate();
+      let futureMonth = date.toLocaleDateString("en-US", { month: "short" });
+      let futureYear = date.getFullYear();
+      let dateToAssert = `${futureMonth} ${futureDay}, ${futureYear}`;
+      cy.get("nb-calendar-navigation")
+        .invoke("attr", "ng-reflect-date")
+        .then((dateAttribute) => {
+          if (
+            !dateAttribute.includes(futureMonth) ||
+            !dateAttribute.includes(futureYear)
+          ) {
+            cy.get("[data-name='chevron-right']").click();
+            selectDayFromCurrent();
+          } else {
+            cy.get(".day-cell")
+              .not("bounding-month")
+              .contains(futureDay)
+              .click();
+          }
+        });
+      return dateToAssert;
+    }
     cy.visit("/");
     cy.contains("Forms").click();
     cy.contains("Datepicker").click();
-
-    let date = new Date();
-    date.setDate(date.getDate() + 5);
-    let futureDate = date.getDate();
-    let dateToAssert = `Oct ${futureDate}, 2024`;
-
     cy.contains("nb-card", "Common Datepicker")
       .find("input")
       .then((input) => {
         cy.wrap(input).click();
-        cy.get(".day-cell").not("bounding-month").contains(futureDate).click();
+        const dateToAssert = selectDayFromCurrent(5);
         cy.wrap(input).invoke("prop", "value").should("contain", dateToAssert);
         cy.wrap(input).should("have.value", dateToAssert);
       });
