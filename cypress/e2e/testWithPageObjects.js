7c8294ae4d86df62d0e707033d55966140446283
alberto-odio-saferidehealth <alberto.odio@saferidehealth.com>
2025-05-22 23:55:50 -0600

    SECTION 5 LESSON 33

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,6 +1,8 @@
-// SECTION 5 LESSON 32
+// SECTION 5 LESSON 33
 import { navigateTo } from "../support/page_objects/navigationPage";
-
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage";
+import { onDatepickerPage } from "../support/page_objects/datepickerPage";
+import { onSmartTablePage } from "../support/page_objects/smartTablePage";
 describe("Test with Page Objects", () => {
   beforeEach("open application", () => {
     cy.visit("/");
@@ -12,5 +14,24 @@ describe("Test with Page Objects", () => {
     navigateTo.smartTablePage();
     navigateTo.toasterPage();
     navigateTo.tooltipPage();
+  });
+
+  it.only("should submit inline form and basic form and select tomorrow date in the calendar", () => {
+    navigateTo.formLayoutsPage();
+    onFormLayoutsPage.submitInlineFormWithNameAndEmail(
+      "Artem",
+      "test@test.com"
+    );
+    onFormLayoutsPage.submitBasicFormWithEmailAndPassword(
+      "test@test.com",
+      "password"
+    );
+    navigateTo.datepickerPage();
+    onDatepickerPage.selectCommonDatepickerDateFromToday(1);
+    onDatepickerPage.selectDatepickerWithRageFromToday(7, 14);
+    navigateTo.smartTablePage();
+    onSmartTablePage.addNewRecordWithFirstAndLastName("Artem", "Bondar");
+    onSmartTablePage.updateAgeByFirstName("Artem", "30");
+    onSmartTablePage.deleteRowByIndex(1);
   });
 });
